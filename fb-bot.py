42324f0fdcb99839d235c1b21b9295a6a54d2dc0
Erik Liodden <eriklio@stud.ntnu.no>
2017-11-25 13:07:15 +0100

    add echobot and dad-jokes
    saitama accepts @echo and @dad. @echo <text> will send back <text> and
    @dad will send back a random dad-joke.

---
+++ fb-bot.py
@@ -0,0 +1,36 @@
+from fbchat import log, Client
+from fbchat.models import *
+import time
+import requests
+
+# Subclass fbchat.Client and override required methods
+class EchoBot(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(author_id, thread_id)
+        self.markAsRead(author_id)
+
+        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+
+        # If Saitama-san is the author -> sayonara
+        if author_id == self.uid:
+            return
+
+        # echo if message start with @echo
+        if message_object.text.startswith('@echo'):
+            self.send(Message(text=message_object.text[6::]),
+                thread_id=thread_id, thread_type=thread_type)
+
+        # send dad joke if message start with @dad
+        if message_object.text.startswith('@dad'):
+            joke = requests.get('https://icanhazdadjoke.com/',
+                    headers={'Accept': 'text/plain'})
+            self.send(Message(text=joke.text),
+                thread_id=thread_id, thread_type=thread_type)
+
+def main():
+    client = EchoBot('kongerik1@gmail.com', 'Qq888888')
+    client.listen()
+    client.logout()
+
+if __name__ == "__main__":
+    main()
